11d5ef33445d6a60955f1b075ec688abf198f1fd
Michael Rafael de Andrade <realteceinformatica@gmail.com>
2024-07-24 22:22:59 -0300

    primeiro commit

---
+++ src/application/Porgram.java
@@ -0,0 +1,23 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Porgram {
+
+	public static void main(String[] args) {
+		// exercicio data hora - 02
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		LocalDate d04 = LocalDate.parse("2022-07-20");
+		
+		
+		sc.close();
+	}
+
+}
